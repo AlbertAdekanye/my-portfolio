4eee706f3052a1136e39ebcf17c8b7ee915f9fd3
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-10-14 12:29:28 +0100

    #

---
+++ src/components/Footer.svelte
@@ -0,0 +1,27 @@
+<footer
+    class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+>
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <div
+            class="flex items-center gap-4 sm:gap-6 text-2xl sm:text-3xl md:text-4xl mx-auto"
+        >
+            <a href="https://www.linkedin.com/in/albert-adekanye-a82118243/">
+                <i class="fa-brands fa-linkedin-in" />
+            </a>
+            <a href="https://github.com/AlbertAdekanye">
+                <i class="fa-brands fa-github" />
+            </a>
+            <a href="https://twitter.com/AlbertAdekanye">
+                <i class="fa-brands fa-twitter" />
+            </a>
+            <a href="https://www.instagram.com/albertadekanye/">
+                <i class="fa-brands fa-instagram" />
+            </a>
+        </div>
+        <p class="mx-auto text-sm opacity-70">or</p>
+        <p class="">adekanyealbert22@gmail.com</p>
+    </div>
+</footer>
